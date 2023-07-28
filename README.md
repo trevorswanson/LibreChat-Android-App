63de238fa048ad7c19486d50322f9259b009b8c9
Air <air220917@gmail.com>
2023-07-28 23:04:08 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,20 +1,61 @@
-# ChatGPT App
+
+<h1 align="center">LibreChat Instance App</h1>
+
+*******
+
 
 ## About
 
-This (unofficial) app acts as a webview wrapper for [chat.openai.com](https://chat.openai.com)
+This app is a webview for [LibreChat](https://github.com/danny-avila/LibreChat) instance Android independent app, this project is forked from [ChatGPT-android-app](https://github.com/matthaigh27/ChatGPT-android-app). Default webpage of this app has been set to LibreChat's GitHub Page. This app is optimized for LibreChat's function which is not in original project. For example, Social Login Oauth login support is added to this build.
 
 
-<div align="center">
-<a href='https://github.com/matthaigh27/ChatGPT-android-app/releases/latest'><img alt='Get it on Github' src='./docs/assets/badge_github.png' height='80px'/></a>
+<div>
+<a href='https://github.com/air220917/ChatGPT-android-app/releases/latest'><img alt='Get it on Github' src='./docs/assets/badge_github.png' height='80px'/></a>
 </div>
 
-## Screenshot
+## Screenshots
 
-<div align="center">
-<img src="./docs/assets/Screenshot_Pixel6pro.png" width="300"  />
+<div>
+<img src="docs/assets/login_page.PNG" width=300><img src="docs/assets/register.PNG" width=300>
+<img src="docs/assets/main.PNG" width=300><img src="docs/assets/sidebar.PNG" width=300>
 </div>
 
-## Installation
 
-Download the latest release and install it on your device.
+## Optimization for LibreChat Web Instance
+
+* Social Login redirects will be done in same webview, no longer jumping to browser
+
+* File uploads support was added to app for the import of presets
+
+<h3>Requirements</h3>
+
+* Android Studio
+## Build
+
+
+   * <h4 style="font-weight: normal;">1. Clone or download the repository.
+```bash
+git clone https://github.com/goodair220917/LibreChat-Android-App.git
+```
+* <h4 style="font-weight: normal;">2. Edit the MainActivity.kt file in /path/to/repo/app/src/main/java/com/librechat/app/ replace the default LibreChat Github repo url with your own LibreChat Instance url.</h4>
+* <h4 style="font-weight: normal;">3. Open Android Studio and open the repository folder, then click the Build button to build the app (app-debug.apk file).
+ </h4>
+
+
+
+
+## Credits
+
+ * <h3><u>LibreChat</u> </h3>
+    <img src="docs/assets/LibreChat.svg" height="80"><a href="https://github.com/danny-avila/LibreChat"><img alt='Get it on Github' src='./docs/assets/badge_github.png' height='70px'/></a>
+    
+    <h3>All-In-One AI Conversations with LibreChat</h3>
+    <hr>
+    <h4>Open-Source ChatGPT Clone supports famous AI providers.</h4>
+    <h6>(LibreChat Project, go check out!!!!)</h6>
+
+ * <h3><u>ChatGPT-android-app</u></h3>
+    <img src="docs/assets/matthaigh27.png" height="80"><a href="https://github.com/matthaigh27/ChatGPT-android-app"><img alt='Get it on Github' src='./docs/assets/badge_github.png' height='70px'/></a>
+   <h4>This (unofficial) app acts as a webview wrapper for <a href="https://chat.openai.com">chat.openai.com</a></h4>
+   <h6>(The original project of this project!)</h6>
+
---
+++ app/src/main/java/com/librechat/app/MainActivity.kt
@@ -0,0 +1,180 @@
+package com.librechat.app
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.webkit.JavascriptInterface
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.window.OnBackInvokedDispatcher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.librechat.app.databinding.ActivityMainBinding
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+
+class MainActivity : AppCompatActivity() {
+    private val userAgent =
+        "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.135 Mobile Safari/537.36"
+    private val chatUrl = "https://github.com/danny-avila/LibreChat"
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var webView: WebView
+    private lateinit var swipeLayout: SwipeRefreshLayout
+    private var fileUploadCallback: ValueCallback<Array<Uri>>? = null
+
+    private val fileChooser =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+                if (data != null) {
+                    val uri = data.data
+                    if (uri != null) {
+                        fileUploadCallback?.onReceiveValue(arrayOf(uri))
+                        fileUploadCallback = null
+                    }
+                }
+            } else {
+                fileUploadCallback?.onReceiveValue(null)
+                fileUploadCallback = null
+            }
+        }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide() // Hide the action bar
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        webView = binding.webView
+        swipeLayout = binding.swipeRefreshLayout
+
+        // Add android:configChanges to handle orientation and screen size changes
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            onBackInvokedDispatcher.registerOnBackInvokedCallback(
+                OnBackInvokedDispatcher.PRIORITY_DEFAULT
+            ) {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    finish()
+                }
+            }
+        }
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.parseColor("#343541")
+
+        webView.settings.userAgentString = userAgent
+        webView.settings.domStorageEnabled = true
+        webView.settings.javaScriptEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+        webView.addJavascriptInterface(WebViewInterface(this), "Android")
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                val url = request?.url ?: return false
+
+                if (url.toString().contains(chatUrl)) {
+                    return false
+                }
+
+                // Load the clicked hyperlink in the current WebView
+                view?.loadUrl(url.toString())
+
+                return false
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                swipeLayout.isRefreshing = false
+                swipeLayout.isEnabled = !(webView.url.toString().contains(chatUrl) &&
+                        !webView.url.toString().contains("/auth"))
+
+                webView.evaluateJavascript(
+                    """
+                    (() => {
+                      navigator.clipboard.writeText = (text) => {
+                            Android.copyToClipboard(text);
+                            return Promise.resolve();
+                        }
+                        
+                      document.querySelector('input[type="file"]').addEventListener('change', (event) => {
+                            Android.openFileChooser();
+                      });
+                    })();
+                    """.trimIndent(),
+                    null
+                )
+            }
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                if (fileUploadCallback != null) {
+                    fileUploadCallback?.onReceiveValue(null)
+                    fileUploadCallback = null
+                }
+
+                fileUploadCallback = filePathCallback
+
+                val contentSelectionIntent = Intent(Intent.ACTION_GET_CONTENT)
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE)
+                contentSelectionIntent.type = "*/*"
+
+                val intentArray: Array<Intent?> = arrayOfNulls(0)
+                val chooserIntent = Intent(Intent.ACTION_CHOOSER)
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent)
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Select File")
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray)
+
+                fileChooser.launch(chooserIntent)
+                return true
+            }
+        }
+
+        swipeLayout.setOnRefreshListener {
+            webView.reload()
+        }
+
+        webView.loadUrl(chatUrl)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        @Suppress("DEPRECATION")
+        if (webView.canGoBack() && Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU)
+            webView.goBack()
+        else
+            super.onBackPressed()
+    }
+
+    private class WebViewInterface(private val context: Context) {
+        @JavascriptInterface
+        fun copyToClipboard(text: String) {
+            val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("Copied!", text)
+
+            clipboard.setPrimaryClip(clip)
+        }
+    }
+}
